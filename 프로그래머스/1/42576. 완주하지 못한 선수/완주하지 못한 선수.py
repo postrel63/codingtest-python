68eefc6aa798102c448c89e22e1c8fe89097ed99
김동욱 <81555158+postrel63@users.noreply.github.com>
2024-05-09 00:02:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.94 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,4 +1,5 @@
 from collections import Counter
 
+
 def solution(participant, completion):
-    return (Counter(participant) - Counter(completion)).most_common().pop()[0]
+    return list(Counter(participant) - Counter(completion))[0]
