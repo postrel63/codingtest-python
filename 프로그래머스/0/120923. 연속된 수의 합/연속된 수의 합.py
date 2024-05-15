15679aeb11f8e3ec397e586a997eafcd32a70380
김동욱 <81555158+postrel63@users.noreply.github.com>
2024-05-15 22:19:54 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -2,5 +2,5 @@ def solution(num, total):
     first = int(total // num - num // 2 ) 
     if num & 1 == 0:
         first += 1  
-    answer = [i for i in range(first,first + num,1)]
-    return answer
+    return [i for i in range(first,first + num,1)]
+     
