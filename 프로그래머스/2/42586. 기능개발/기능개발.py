2d038c0ce6a55141d169605357015f6bc80f3019
김동욱 <81555158+postrel63@users.noreply.github.com>
2024-05-19 22:45:40 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,8 +1,3 @@
- # while 문으로 시간 day 똑같이 반복
-# for 문으로 progresses 반복하면서 progresses + speeds x day >= 100 이면 정지
-# progresses의 앞순서의 진도율이 100이 넘는 걸 매번 체크해서 넘는 순간에 그 작업과 연속된 뒤의 작업들까지 모두 answer에 담아줌
-# 다시 반복
-
 
 # or 각 항목마다 며칠뒤에 끝나는지 리스트로 다 만들고 한번에 계산
 import math
