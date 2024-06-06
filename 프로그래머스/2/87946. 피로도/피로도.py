c2836636e48494e5ec66621cf00ffdda26419131
김동욱 <81555158+postrel63@users.noreply.github.com>
2024-06-06 19:39:34 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -0,0 +1,14 @@
+def dfs(k, cnt, dungeons, visited):
+    maxCount = cnt
+    
+    for i in range(len(dungeons)):
+        if not visited[i] and k >= dungeons[i][0]:
+            visited[i] = True
+            maxCount = max(maxCount, dfs(k - dungeons[i][1], cnt + 1, dungeons, visited))
+            visited[i] = False
+    
+    return maxCount
+        
+def solution(k, dungeons):
+    visited = [False] * len(dungeons)
+    return dfs(k, 0, dungeons, visited)
