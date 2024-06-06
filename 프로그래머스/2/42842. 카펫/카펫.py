60efc77004a41d25263ac2813ca521da47343cd6
김동욱 <81555158+postrel63@users.noreply.github.com>
2024-06-06 18:29:16 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -3,11 +3,12 @@
 # yellow = (width / 2) * (height / 2) 
 def solution(brown, yellow):
     answer = []
+    total = brown + yellow
 
-    for i in range(3, brown + yellow):
+    for i in range(3, total):
         length = i
-        width = int((brown + yellow) / length)
-        if length * width != brown + yellow:
+        width = int(total / length)
+        if length * width != total:
             continue
 
         if brown == (length * 2) + (width * 2) - 4:
