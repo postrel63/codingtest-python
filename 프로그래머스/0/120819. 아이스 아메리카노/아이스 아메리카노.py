1f4ae820b4a7897013eb2b6e5f4048750b218abb
김동욱 <81555158+postrel63@users.noreply.github.com>
2024-11-02 19:05:21 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,7 +1,5 @@
 import math
 def solution(money):
     answer = []
-    americano = 5500
-    answer = [math.floor(money / americano) , money % americano]
-    
-    return answer
+    americano = 5500   
+    return [money // 5500, money % 5500]
