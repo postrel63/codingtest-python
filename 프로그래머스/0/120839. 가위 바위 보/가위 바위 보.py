320f256b40fcb431f92b09ec689641af4c9c300f
김동욱 <81555158+postrel63@users.noreply.github.com>
2024-11-02 20:54:28 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,9 +1,5 @@
-# 2 < 0 
-# 0 < 5
-# 5 < 2
 def solution(rsp):
     answer = ''
-    list = [2, 0, 5]
     for i in rsp:
         if (i == '2'):
             answer += '0'
@@ -11,5 +7,4 @@ def solution(rsp):
             answer += '5'
         else:
             answer += '2'
-    
     return answer
