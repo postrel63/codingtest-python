c2669605dff01f6bb7da0c77a2966ec1e2ba5732
김동욱 <81555158+postrel63@users.noreply.github.com>
2024-11-06 22:49:26 +0900

    [level 1] Title: 약수의 합, Time: 0.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,11 +1,6 @@
 def solution(n):
     answer = 0
-    if (0 > n and 3000 < n):
-        return 0
-    
     for i in range(1, n + 1):
         if n % i == 0:
-            answer += i
-            
-        
+            answer += i    
     return answer
